eea5b30910de43357b5d124fe264869b67161a6d
URGG <georgeurgiles2@gmail.com>
2025-06-11 11:21:43 -0700

    as

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function showIosInstructions() {
+// Automatically show instructions if on iOS and not in standalone mode
+window.addEventListener('load', () => {
+  const isIos = /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
+  const isInStandaloneMode = 'standalone' in window.navigator && window.navigator.standalone;
+
+  if (isIos && !isInStandaloneMode) {
     document.getElementById('iosPrompt').style.display = 'block';
   }
+});
