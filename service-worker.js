98fc3afa5432778d37320399294805b5cea264af
URGG <georgeurgiles2@gmail.com>
2025-06-10 21:35:26 -0700

    service worker

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,23 @@
 self.addEventListener('install', (e) => {
-    console.log('Service worker installed');
-  });
-  
-  self.addEventListener('fetch', () => {});
-  
+  e.waitUntil(
+    caches.open('leslie-card-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json',
+        '/les-digital-bc.jpg',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png',
+        '/icons/apple-icon-180.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
